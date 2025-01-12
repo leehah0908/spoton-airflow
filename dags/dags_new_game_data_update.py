1cabf7e71101a49e2a055d999b722c1a993d0a74
leehah <leehah0908@naver.com>
2025-01-13 00:13:03 +0900

    클론 후 필요없는 파일 삭제

--- dags/dags_new_game_data_update.py
+++ dags/dags_new_game_data_update.py
@@ -2,8 +2,10 @@ from operators.new_game_data_operator import NewGameDataOperator
 from airflow import DAG
 import pendulum
 
+# 새로운 경기 일정 업데이트
 with DAG(
     dag_id='dags_new_game_data_update',
+    # 매일 00:00 실행
     schedule='0 0 * * *',
     start_date=pendulum.datetime(2025, 1, 8, tz='Asia/Seoul'),
     catchup=False
