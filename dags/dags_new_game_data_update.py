430996bd9c3eec064c4b04deb9980dd275d4ec16
leehah <leehah0908@naver.com>
2025-01-12 22:36:10 +0900

    aitaction test

--- dags/dags_new_game_data_update.py
+++ dags/dags_new_game_data_update.py
@@ -2,10 +2,8 @@ from operators.new_game_data_operator import NewGameDataOperator
 from airflow import DAG
 import pendulum
 
-# 신규 경기 일정 업데이트
 with DAG(
     dag_id='dags_new_game_data_update',
-    # 매일 00:00 실행
     schedule='0 0 * * *',
     start_date=pendulum.datetime(2025, 1, 8, tz='Asia/Seoul'),
     catchup=False
