0d67fe932dab4585ff090a9509377ba0f79212c8
leehah <leehah0908@naver.com>
2025-02-03 20:50:41 +0900

    최신 경기 일정 업데이트 배치 시간 수정

--- dags/dags_new_game_data_update.py
+++ dags/dags_new_game_data_update.py
@@ -5,8 +5,8 @@ import pendulum
 # 새로운 경기 일정 업데이트
 with DAG(
     dag_id='dags_new_game_data_update',
-    # 매일 00:00 실행
-    schedule='0 0 * * *',
+    # 매일 12:00 실행
+    schedule='0 12 * * *',
     start_date=pendulum.datetime(2025, 1, 8, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
