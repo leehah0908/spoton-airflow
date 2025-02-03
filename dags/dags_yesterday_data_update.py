9d857c1ac385749b50eea6282301e7f2c53a9fc2
leehah <leehah0908@naver.com>
2025-02-03 20:50:59 +0900

    어제 경기 데이터 수집 배치 시간 수정

--- dags/dags_yesterday_data_update.py
+++ dags/dags_yesterday_data_update.py
@@ -5,8 +5,8 @@ import pendulum
 # 어제 경기 결과 재수집
 with DAG(
     dag_id='dags_yesterday_data_update',
-    # 매일 00:10 실행
-    schedule='10 0 * * *',
+    # 매일 00:00 실행
+    schedule='0 0 * * *',
     start_date=pendulum.datetime(2025, 1, 8, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
